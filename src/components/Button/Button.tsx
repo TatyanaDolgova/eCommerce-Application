6c5e0ab66535264fa7ebabedd927e9ca5943c892
jofetta <jafarovatatiana@gmail.com>
2024-05-05 15:31:28 +0300

    feat/implement_basic_button_and_input

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,22 @@
+import { MouseEventHandler } from 'react';
+
+function BaseButton(props: ButtonProps) {
+  return (
+    <button
+      className={props.classes}
+      type={props.type}
+      onClick={props.callback}
+    >
+      {props.text}
+    </button>
+  );
+}
+
+interface ButtonProps {
+  callback: MouseEventHandler;
+  classes: string;
+  text: string;
+  type: 'button' | 'submit' | 'reset' | undefined;
+}
+
+export default BaseButton;

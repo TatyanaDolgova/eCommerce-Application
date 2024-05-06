63fcabb6fbf2cbc25f675aae2c06674f9ca65a12
Tatyana Dolgova <kylikovatatyana1993@mail.ru>
2024-05-06 11:34:26 +0500

    feat: implement header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,29 @@
+import { Link } from 'react-router-dom';
+
+import './Header.css';
+
 const Header = () => {
-  return <header className="header">Header</header>;
+  return (
+    <header className="header">
+      <Link className="header-logo" to="/">
+        Platify
+      </Link>
+      <nav>
+        <ul className="header-links">
+          <li>
+            <Link className="header-link" to="/login">
+              Login
+            </Link>
+          </li>
+          <li>
+            <Link className="header-link" to="/registration">
+              Registration
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;

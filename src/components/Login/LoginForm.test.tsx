49b240a8fd70f0e5b288496f439114147dd1ba4d
Maria Treier <mariatraier@gmail.com>
2024-05-07 12:01:23 +0200

    chore: update test libraries

---
+++ src/components/Login/LoginForm.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+
+import LoginForm from './LoginForm';
+
+test('should have password type by default', () => {
+  render(<LoginForm />);
+  const passwordInput = screen.getByPlaceholderText(/Type your password/i);
+
+  expect(passwordInput).toBeInTheDocument();
+  expect(passwordInput.getAttribute('type')).toBe('password');
+});
+
+// test('should have text type after checking input', () => {
+//   const result = render(<LoginForm />);
+//   const passwordInput = result.container.querySelector('.show_password_input');
+
+//   expect(passwordInput).toBeInTheDocument();
+//   expect(passwordInput.getAttribute('type')).toBe('password');
+// });

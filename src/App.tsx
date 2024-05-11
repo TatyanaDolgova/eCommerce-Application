2a6788abdd0e9a0a962c1bc6bb85c2c69a76f395
Tatyana Dolgova <kylikovatatyana1993@mail.ru>
2024-05-11 17:19:13 +0500

    Feat/implement not found page (#25)
    * feat:implement not found page
    
    * feat:added image and style for not found page
    
    * Update src/pages/NotFoundPage/NotFoundPage.tsx
    
    Co-authored-by: Jofetta <70841239+Jofetta@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Jofetta <70841239+Jofetta@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { LoginPage } from './pages/login/LoginPage';
 import MainPage from './pages/MainPage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 import RegistrationPage from './pages/RegistrationPage';
 import { CustomerRepository } from './services/CustomerRepository';
 
@@ -12,9 +13,11 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="*" element={<MainPage />} />
+        <Route path="/" element={<MainPage />} />
+        <Route path="/home" element={<MainPage />} />
         <Route path="/login" element={<LoginPage apiRoot={apiRoot} />} />
         <Route path="/registration" element={<RegistrationPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );

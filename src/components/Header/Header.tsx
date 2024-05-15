12022b9210734b31900bbf408b73911979d8063e
Tatyana Dolgova <kylikovatatyana1993@mail.ru>
2024-05-15 08:19:04 +0500

    feat: implement burger menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,5 @@
+import { Squash as Hamburger } from 'hamburger-react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import './Header.css';
@@ -5,16 +7,18 @@ import { CustomerRepository } from '../../services/CustomerRepository';
 import BaseButton from '../Button/Button';
 
 const Header = () => {
+  const [isOpen, setOpen] = useState(false);
+
   return (
     <header className="header">
       <div className="header-wrapper">
         <Link className="header-logo" to="/">
           Plantify
         </Link>
-        <Link to="/home" className="header-link">
-          Home
-        </Link>
-        <nav>
+        <nav className="header-nav">
+          <Link to="/home" className="header-link">
+            Home
+          </Link>
           <ul className="header-links">
             <li>
               <Link className="header-link sign-in-link" to="/login">
@@ -27,15 +31,18 @@ const Header = () => {
               </Link>
             </li>
           </ul>
+          <BaseButton
+            classes="log_out_button header-link"
+            callback={async () => {
+              await CustomerRepository.logOutCusromer();
+            }}
+            text="Log out"
+            type="button"
+          ></BaseButton>
         </nav>
-        <BaseButton
-          classes="log_out_button header-link"
-          callback={async () => {
-            await CustomerRepository.logOutCusromer();
-          }}
-          text="Log out"
-          type="button"
-        ></BaseButton>
+        <div className="header-hidden">
+          <Hamburger toggled={isOpen} size={35} toggle={setOpen} color="#fff" />
+        </div>
       </div>
     </header>
   );

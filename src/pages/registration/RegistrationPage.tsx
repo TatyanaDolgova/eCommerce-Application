4817742d93220e94e873fbc0cda39ac98feac606
Jofetta <70841239+Jofetta@users.noreply.github.com>
2024-05-16 11:04:47 +0300

    feat: implement unit tests for registration page (#42)

--- src/pages/registration/RegistrationPage.tsx
+++ src/pages/registration/RegistrationPage.tsx
@@ -1,5 +1,5 @@
-import Header from '../components/Header/Header';
-import RegistrationForm from '../components/Registration/RegistrationForm';
+import Header from '../../components/Header/Header';
+import RegistrationForm from '../../components/Registration/RegistrationForm';
 
 const RegistrationPage = () => {
   return (

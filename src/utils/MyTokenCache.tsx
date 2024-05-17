44d699000063246e6a29dea2777e5d801b2e6862
Tatyana Dolgova <kylikovatatyana1993@mail.ru>
2024-05-17 18:23:28 +0500

    feat: get token (#54)

---
+++ src/utils/MyTokenCache.tsx
@@ -0,0 +1,27 @@
+import {
+  TokenCache,
+  TokenCacheOptions,
+  TokenStore,
+} from '@commercetools/sdk-client-v2';
+
+class MyTokenCache implements TokenCache {
+  private tokenStore: TokenStore;
+
+  constructor() {
+    this.tokenStore = {
+      token: '',
+      expirationTime: 0,
+      refreshToken: '',
+    };
+  }
+
+  get(): TokenStore {
+    return this.tokenStore;
+  }
+
+  set(cache: TokenStore): void {
+    this.tokenStore = cache;
+  }
+}
+
+export default new MyTokenCache();

851d1a16f19d0d95cbbfd0338929ad564f956a75
jofetta <jafarovatatiana@gmail.com>
2024-05-20 18:02:45 +0300

    feat: create user profile page

---
+++ src/pages/UserProfilePage/UserProfilePage.tsx
@@ -0,0 +1,14 @@
+import Header from '../../components/Header/Header';
+
+const UserProfilePage = () => {
+  return (
+    <div>
+      <Header></Header>
+      <main className="login_page">
+        <h1>User Profile</h1>
+      </main>
+    </div>
+  );
+};
+
+export default UserProfilePage;

7cdbcf57579805d1e5707b302e2059014f0cc503
Maria Treier <mariatraier@gmail.com>
2024-05-30 17:46:00 +0200

    feat: implement modal slider

---
+++ src/components/DetailedProduct/ProductImageModal.tsx
@@ -0,0 +1,43 @@
+import { Image } from '@commercetools/platform-sdk';
+import { A11y, Navigation, Pagination, Scrollbar } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import BaseButton from '../Button/Button';
+
+import '../UserProfile/Modal.css';
+
+interface SliderProps {
+  closeModal: () => void;
+  slides: Image[];
+}
+
+export const ModalProductImage = ({ slides, closeModal }: SliderProps) => {
+  return (
+    <div className="shadow">
+      <div className="modal-container">
+        <BaseButton
+          classes="button buttonX"
+          text="X"
+          type="button"
+          callback={closeModal}
+        />
+        <Swiper
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          spaceBetween={50}
+          slidesPerView={1}
+          navigation
+          pagination={{ clickable: true, type: 'fraction' }}
+          scrollbar={{ draggable: true }}
+        >
+          {slides.map((slide) => (
+            <SwiperSlide key={slide.url}>
+              <div className="swiper-zoom-container">
+                <img src={slide.url} alt="product_picture" />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
+};
--- src/components/DetailedProduct/Slider.tsx
+++ src/components/DetailedProduct/Slider.tsx
@@ -1,31 +1,50 @@
 import { Image } from '@commercetools/platform-sdk';
-import { A11y, Navigation, Pagination, Scrollbar, Zoom } from 'swiper/modules';
+import { useState } from 'react';
+import { A11y, Navigation, Pagination, Scrollbar } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 import 'swiper/css/bundle';
+import { ModalProductImage } from './ProductImageModal';
 
 interface SliderProps {
   slides: Image[];
 }
 
 export const MySlider = ({ slides }: SliderProps) => {
-  return (
-    <Swiper
-      modules={[Navigation, Pagination, Scrollbar, A11y, Zoom]}
-      spaceBetween={50}
-      slidesPerView={1}
-      zoom={true}
-      navigation
-      pagination={{ clickable: true, type: 'fraction' }}
-      scrollbar={{ draggable: true }}
-    >
-      {slides.map((slide) => (
-        <SwiperSlide key={slide.url}>
-          <div className="swiper-zoom-container">
-            <img src={slide.url} alt="product_picture" />
-          </div>
-        </SwiperSlide>
-      ))}
-    </Swiper>
-  );
+  const [modalSliderOpen, setModalSlider] = useState(false);
+
+  const showModal = () => {
+    setModalSlider(true);
+  };
+
+  const closeModal = () => {
+    setModalSlider(false);
+  };
+
+  if (modalSliderOpen) {
+    return <ModalProductImage slides={slides} closeModal={closeModal} />;
+  } else {
+    return (
+      <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={50}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true, type: 'fraction' }}
+        scrollbar={{ draggable: true }}
+      >
+        {slides.map((slide) => (
+          <SwiperSlide key={slide.url}>
+            <div className="swiper-zoom-container">
+              <img
+                src={slide.url}
+                alt="product_picture"
+                onClick={() => showModal()}
+              />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    );
+  }
 };

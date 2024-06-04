b4d3d3539edfe99de7666cd56d60cf5a8ee3abc3
Tatyana Dolgova <kylikovatatyana1993@mail.ru>
2024-06-04 11:47:07 +0500

    feat: implement add to card button (#102)

--- src/components/Catalog/ProductList/ProductList.tsx
+++ src/components/Catalog/ProductList/ProductList.tsx
@@ -5,15 +5,20 @@ import ProductCard from '../ProductCard/ProductCard';
 import './ProductList.css';
 
 interface ProductListProps {
+  onAddToCart: (productId: string) => void;
   products: ProductProjection[];
 }
 
-const ProductList: React.FC<ProductListProps> = ({ products }) => {
+const ProductList: React.FC<ProductListProps> = ({ products, onAddToCart }) => {
   return (
     <>
       <div className="products-container">
         {products.map((product) => (
-          <ProductCard key={product.id} product={product} />
+          <ProductCard
+            key={product.id}
+            product={product}
+            onAddToCart={onAddToCart}
+          />
         ))}
       </div>
     </>
